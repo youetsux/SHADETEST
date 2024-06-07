ba2f05ee5185511353b896ca3809ee44a6bb7bcc
陽悦 佐藤 <youetsux@gmail.com>
2024-06-07 11:22:16 +0900

    もうめんどくさいからプロジェクトフォルダにライブラリおいてみた

--- Direct3d.cpp
+++ Direct3d.cpp
@@ -264,7 +264,8 @@ HRESULT Direct3D::InitShader()
 void Direct3D::BeginDraw()
 {
 	//”wŒi‚ÌF
-	float clearColor[4] = { 255/255.0f, 250/255.0f, 205/255.0f, 1.0f };//R,G,B,A
+	//float clearColor[4] = { 255/255.0f, 250/255.0f, 205/255.0f, 1.0f };//R,G,B,A
+	float clearColor[4] = { 0.5, 0.5, 0.1, 1.0f };//R,G,B,A
 
 	//‰æ–Ê‚ðƒNƒŠƒA
 	pContext->ClearRenderTargetView(pRenderTargetView.Get(), clearColor);
